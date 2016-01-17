1008b804555fd9335fb4de719d44b783192b41a1
xianvv <chaoqianvv@126.com>
2016-01-17 21:12:11 +0800

    second test
    second

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 var http = require("http");
 http.createServer(function(request, response) {
+	var fs = require("fs");
+	var data = fs.readFileSync('README.md');
 	//发送HTTP头部
 	//HTTP状态值：200：OK
 	//内容类型：text/plain
 	response.writeHead(200, {
 		'Content-Type': 'text/plain'
 	});
-
+	response.write('Hello \n' + data.toString());
 	//发送响应数据
-	response.end('Hello \n');
+	response.end();
 }).listen(8888);
 
 //终端打印
